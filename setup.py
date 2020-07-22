03dfc6ec175ba2edb873ac3defd8a4d5d69e3ea0
Duncan Koss <dkoss@fb.com>
2020-07-21 23:34:00 -0700

    Bumping version to 0.2.0 to see if this removes weird artifacts I'm seeing when running pip install

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="serverless-aurora",
-    version="0.1.1",
+    version="0.2.0",
     url='https://github.com/duncankoss/serverless-aurora',
     license='Apache Software License',
     author='dkoss',
